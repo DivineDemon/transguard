2f4a5eb3b9ee0df370e1a2de1cd228a402f6aef8
DivineDemon <supame123@gmail.com>
2022-04-10 00:12:26 +0400

    :hammer:

--- config/db.js
+++ config/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(
+      "mongodb+srv://adminUser:adminUser@testlocal.o2edm.mongodb.net/theguardman?retryWrites=true&w=majority"
+    );
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
